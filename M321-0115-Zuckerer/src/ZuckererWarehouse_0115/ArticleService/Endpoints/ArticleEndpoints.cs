76f1eba52e8b708315053b43966df85aac5dbcf1
Giovanni <111045802+Giovi06@users.noreply.github.com>
2024-11-04 11:05:44 +0100

    CQRS-Architektur

--- M321-0115-Zuckerer/src/ZuckererWarehouse_0115/ArticleService/Endpoints/ArticleEndpoints.cs
+++ M321-0115-Zuckerer/src/ZuckererWarehouse_0115/ArticleService/Endpoints/ArticleEndpoints.cs
@@ -10,66 +10,114 @@ namespace ArticleService.Endpoints
         {
             var group = routes.MapGroup("/Article").WithTags(nameof(Article));
 
-            group.MapGet("", async (ArticleDb db) =>
-                await db.Articles.ToListAsync()
+            group.MapGet("/List", async (ArticleDb db) =>
+                await db.Articles.Select(x => ListArticlesDTO.FromArticle(x)).ToListAsync()
             )
             .WithName("List")
             .WithOpenApi();
 
-            group.MapGet("/{id}", async (string id, ArticleDb db) =>
-            {
-                if (Guid.TryParse(id, out var guidId))
-                {
-                    var article = await db.Articles.FindAsync(guidId);
-                    return article != null ? Results.Ok(article) : Results.NotFound();
-                }
-                else
-                {
-                    return Results.BadRequest("Invalid GUID format.");
-                }
-            }
+            group.MapGet("/GetById/{id}", async (string id, ArticleDb db) =>
+                await db.Articles.FindAsync(id)
+                    is Article article
+                        ? Results.Ok(GetArticleDTO.FromArticle(article))
+                        : Results.NotFound()
             )
             .WithName("GetArticleById")
             .WithOpenApi();
 
-            group.MapPost("", async (Article dto, ArticleDb db) =>
+            group.MapPost("/New", async (NewArticleDTO dto, ArticleDb db, MqSender mq) =>
             {
-                var dbObj = new Article { ArticleNumber = dto.ArticleNumber, ShortDesc = dto.ShortDesc, Price = dto.Price };
+                var dbObj = new Article { ArticleNumber = dto.ArticleNumber, ShortDesc = dto.ShortDescription, Price = dto.Price };
                 db.Articles.Add(dbObj);
                 await db.SaveChangesAsync();
-                return Results.Created($"/{dbObj.Id.ToString()}", dbObj);
+                mq.SendAsJson("ArticleService.Command.NewArticle", new { dbObj.Id, dto.ArticleNumber, dto.ShortDescription, dto.Price });
+                return Results.Created($"/GetArticleById/{dbObj.Id.ToString()}", GetArticleDTO.FromArticle(dbObj));
             })
             .WithName("NewArticle")
             .WithOpenApi();
 
-            group.MapPut("/{id}", async (Guid id, Article dto, ArticleDb db) =>
+            group.MapPost("/Update/{id}", async (Guid id, UpdateArticleDTO dto, ArticleDb db, MqSender mq) =>
             {
                 var dbObj = await db.Articles.FindAsync(id);
                 if (dbObj == null)
                 {
                     return Results.NotFound();
                 }
+                dbObj.ShortDesc = dto.ShortDescription;
+                dbObj.Price = dto.Price;
                 await db.SaveChangesAsync();
+                mq.SendAsJson("ArticleService.Command.UpdateArticle", new { id, dto.CategoryId, dto.ShortDescription, dto.LongDescription, dto.Price, dto.Weight, dto.PackageDimensionH, dto.PackageDimensionL, dto.PackageDimensionW });
                 return Results.Ok();
             })
             .WithName("UpdateArticle")
             .WithOpenApi();
 
-            group.MapDelete("/{id}", async (Guid id, ArticleDb db) =>
+            group.MapPost("/ProcessGoodsIn", async (GoodsInDTO dto, ArticleDb db, MqSender mq) =>
             {
-                var dbObj = await db.Articles.FindAsync(id);
-                if (dbObj == null)
+                foreach (var receivedGood in dto.ReceivedGoods)
                 {
-                    return Results.NotFound();
+                    var article = await db.Articles.FindAsync(receivedGood.Id);
+                    if (article == null)
+                    {
+                        return Results.BadRequest();
+                    }
+                    article.QuantityInStock += receivedGood.Quantity;
                 }
-                db.Articles.Remove(dbObj);
                 await db.SaveChangesAsync();
+                mq.SendAsJson("ArticleService.Command.ProcessGoodsIn", dto);
                 return Results.Ok();
             })
-            .WithName("DeleteArticle")
+            .WithName("ProcessGoodsIn")
+            .WithOpenApi();
+
+            group.MapPost("/ProcessGoodsOut", async (GoodsOutDTO dto, ArticleDb db, MqSender mq) =>
+            {
+                using var tx = await db.Database.BeginTransactionAsync();
+
+                var article = await db.Articles.FindAsync(dto.ArticleId);
+                if (article == null)
+                {
+                    return Results.BadRequest();
+                }
+
+                if (article.QuantityInStock < dto.Quantity)
+                {
+                    return Results.Conflict("Not enough goods in stock");
+                }
+                article.QuantityInStock -= dto.Quantity;
+
+                Console.WriteLine($"ProcessGoodsOut: ArticleId={dto.ArticleId} Quantity={dto.Quantity} OrderId={dto.OrderId}");
+                await db.SaveChangesAsync();
+                mq.SendAsJson("ArticleService.Command.ProcessGoodsOut", dto);
+                await tx.CommitAsync();
+                return Results.Ok();
+            })
+            .WithName("ProcessGoodsOut")
             .WithOpenApi();
         }
     }
+
+    public record ListArticlesDTO(string id, string ArticleNumber, string ShortDescription, int Price)
+    {
+        public static ListArticlesDTO FromArticle(Article article)
+        {
+            return new ListArticlesDTO(article.Id.ToString().ToLower(), article.ArticleNumber, article.ShortDesc, article.Price);
+        }
+    }
+
+    public record GetArticleDTO(string id, string ArticleNumber, string ShortDescription, string LongDescription, int Price)
+    {
+        public static GetArticleDTO FromArticle(Article article)
+        {
+            return new GetArticleDTO(
+                article.Id.ToString().ToLower(),
+                article.ArticleNumber,
+                article.ShortDesc,
+                article.LongDesc ?? "",
+                article.Price);
+        }
+    };
+
     public record NewArticleDTO(string ArticleNumber, string ShortDescription, int Price);
 
     public record UpdateArticleDTO(string ShortDescription, int Price, string? LongDescription, Guid? CategoryId, int? Weight, int? PackageDimensionW, int? PackageDimensionL, int? PackageDimensionH);
